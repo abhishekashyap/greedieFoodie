56793dad7669c43c908b3c579b5c776b852a457c
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-03-02 19:01:44 +0530

    Added cards

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,82 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+  <v-app id="foodie">
+    <v-app-bar app clipped-left class="primary">
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <span class="title ml-3 mr-5">
+        Greedie&nbsp;
+        <span class="font-weight-light">Foodie</span>
+      </span>
+      <v-text-field
+        solo-inverted
+        flat
+        hide-details
+        label="Search"
+        prepend-inner-icon="search"
+      />
+      <v-spacer />
     </v-app-bar>
 
+    <v-navigation-drawer v-model="drawer" app clipped color="grey lighten-4">
+      <v-list dense class="grey lighten-4">
+        <template v-for="(item, i) in items">
+          <v-row v-if="item.heading" :key="i" align="center">
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+            </v-col>
+            <v-col cols="6" class="text-right">
+              <v-btn small text>edit</v-btn>
+            </v-col>
+          </v-row>
+          <v-divider v-else-if="item.divider" :key="i" dark class="my-4" />
+          <v-list-item v-else :key="i" link>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title class="grey--text">{{
+                item.text
+              }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-content>
-      <HelloWorld />
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
-  name: "App",
-
-  components: {
-    HelloWorld
+  props: {
+    source: String
   },
-
   data: () => ({
-    //
+    drawer: null,
+    items: [
+      { icon: "lightbulb_outline", text: "Notes" },
+      { icon: "touch_app", text: "Reminders" },
+      { divider: true },
+      { heading: "Labels" },
+      { icon: "add", text: "Create new label" },
+      { divider: true },
+      { icon: "archive", text: "Archive" },
+      { icon: "delete", text: "Trash" },
+      { divider: true },
+      { icon: "settings", text: "Settings" },
+      { icon: "chat_bubble", text: "Trash" },
+      { icon: "help", text: "Help" },
+      { icon: "phonelink", text: "App downloads" },
+      { icon: "keyboard", text: "Keyboard shortcuts" }
+    ]
   })
 };
 </script>
+
+<style>
+#foodie .v-navigation-drawer__border {
+  display: none;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-card class="mx-auto my-12" max-width="374">
+    <v-img height="250" :src="restraunt.image"></v-img>
+
+    <v-card-title>{{ restraunt.name }}</v-card-title>
+
+    <v-card-text>
+      <v-row align="center" class="mx-0">
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+
+        <div class="grey--text ml-4">4.5 (413)</div>
+      </v-row>
+
+      <div class="my-4 subtitle-1">
+        $ {{ restraunt.price }} • {{ restraunt.class }}
+      </div>
+
+      <div>
+        {{ restraunt.details }}
+      </div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-title>{{ restraunt.open }}</v-card-title>
+
+    <v-card-text>
+      <v-chip-group active-class="deep-purple accent-4 white--text" column>
+        <v-chip>5:30PM</v-chip>
+
+        <v-chip>7:30PM</v-chip>
+
+        <v-chip>8:00PM</v-chip>
+
+        <v-chip>9:00PM</v-chip>
+      </v-chip-group>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="deep-purple lighten-2" text>
+        Book now
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="deep-purple" text>
+        View menu
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["restraunt"]
+};
+</script>
+
+<style></style>
